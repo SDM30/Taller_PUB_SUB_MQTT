46d67c1ef0478eb71d3e61fec93287580d8dc309
KillerZeus666 <katheryng2017@gmail.com>
2025-04-28 13:46:49 -0500

    Agregando casos de qr para Pruebas

---
+++ qr.py
@@ -0,0 +1,23 @@
+import random
+import barcode
+from barcode.writer import ImageWriter
+
+# Establecer los primeros 5 dígitos de 1 a 5, pero comenzando con 1
+first_digits = '12345'
+
+# Los siguientes 7 dígitos se generan aleatoriamente
+remaining_digits = ''.join(str(random.randint(0, 9)) for _ in range(7))
+
+# El número completo EAN-13
+ean_base = first_digits + remaining_digits
+
+# Crear código EAN-13 (el paquete calcula automáticamente el dígito de control)
+ean = barcode.get('ean13', ean_base, writer=ImageWriter())
+
+# Guardar el EAN-13 en un archivo
+filename = 'ean13_code'
+full_path = ean.save(filename)
+
+# Mostrar el código base y la ubicación del archivo
+print('Código base:', ean_base)
+print('Guardado en:', full_path)
